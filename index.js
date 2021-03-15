11716b720ede7e86b0e9e0f5bd090c2fa115de9d
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-03-15 18:59:42 -0400

    commit tree 27273d6787c6478073038f20c5693ae0f0162001

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time){
+  
+  const hour = parseInt(time, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+
+}
+
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg
+}
